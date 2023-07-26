061b5ce16a91d6426e6b6cd74627b2a88a0cedad
Felipe Graeff <felipe.graeff@sap.com>
2023-07-26 19:05:43 -0300

    app

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def get_answer(message: str, user_talking: str):
                 'content': message
             }
         ],
-        functions=list(map(completion_functions, lambda x: x.description))
+        functions=list(map(completion_functions.value(), lambda x: x['description']))
     )
     if completion.choices[0].finish_reason == 'function_call':
         function_call = completion.choices[0].message.function_call
