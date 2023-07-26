9cf8ba595d9bf83eadac43a3bd76a2baa0a6fd78
Felipe Graeff <felipe.graeff@sap.com>
2023-07-26 19:39:51 -0300

    app

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def get_answer(message: str, user_talking: str):
         completion_function = completion_functions.get(function_call.name)
         # arguments = json.dumps(function_call.arguments)
         print(function_call.arguments)
-        return completion_function['function'](function_call.arguments)
+        return completion_function['function'](*function_call.arguments)
     else:
         return completion.choices[0].message.content
 
